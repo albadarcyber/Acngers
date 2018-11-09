b9054078d4ce2f99086766637f8678e00495beea
albadarcyber <42489512+albadarcyber@users.noreply.github.com>
2018-11-09 03:16:09 -0800

    Update gmail.py

--- Brutrforce/gmail.py
+++ Brutrforce/gmail.py
@@ -15,7 +15,7 @@ def slowprint(s):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(2.0 / 90)
-
+clear()
 slowprint ('='*45)
 slowprint ('Welcome To Albadar Cyber')
 slowprint ("Bruteforce Gmail")
